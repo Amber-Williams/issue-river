89179800a168a5abf081e4bd0ed10a86d75ad306
Amber-Williams <amberswebdesign@gmail.com>
2021-10-24 00:18:47 +0100

    test: Workspace and User endpoint tests

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/users/{user_id}", response_model=schemas.User)
+@app.get("/user/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db=db, user_id=user_id)
     if db_user is None:
@@ -29,21 +29,21 @@ def read_users(workspace_id: int, db: Session = Depends(get_db)):
     users = crud.get_users_by_workspace(db=db, workspace_id=workspace_id)
     return users
 
-@app.post("/users", response_model=schemas.User)
+@app.post("/user", response_model=schemas.User)
 def create_user(user_create: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db=db, email=user_create.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user_create=user_create)
 
-@app.get("/workspaces", response_model=schemas.Workspace)
+@app.get("/workspace/{workspace_id}", response_model=schemas.Workspace)
 def read_workspace(workspace_id: int, db: Session = Depends(get_db)):
     db_workspace = crud.get_workspace(db=db, workspace_id=workspace_id)
     if db_workspace is None:
         raise HTTPException(status_code=404, detail="Workspace not found")
     return db_workspace
 
-@app.post("/workspaces", response_model=schemas.Workspace)
+@app.post("/workspace", response_model=schemas.Workspace)
 def create_workspace(workspace_create: schemas.WorkspaceCreate, db: Session = Depends(get_db)):
     db_workspace = crud.get_workspace_by_name(db=db, workspace_name=workspace_create.name)
     if db_workspace:
@@ -60,7 +60,7 @@ def get_workspace_edit(db: Session, workspace_update: schemas.WorkspaceEdit):
 
     return [ db_user, db_workspace ]
 
-@app.put("/workspaces/user", response_model=schemas.Workspace)
+@app.put("/workspace/user", response_model=schemas.Workspace)
 def add_user_to_workspace(workspace_update: schemas.WorkspaceEdit, db: Session = Depends(get_db)):
     db_user, db_workspace = get_workspace_edit(db=db, workspace_update=workspace_update)
     for user in db_workspace.users:
@@ -68,7 +68,7 @@ def add_user_to_workspace(workspace_update: schemas.WorkspaceEdit, db: Session =
             raise HTTPException(status_code=400, detail="User already registered to workspace")
     return crud.add_user_to_workspace(db=db, db_user=db_user, db_workspace=db_workspace)
 
-@app.delete("/workspaces/user", response_model=schemas.Workspace)
+@app.delete("/workspace/user", response_model=schemas.Workspace)
 def delete_user_from_workspace(workspace_update: schemas.WorkspaceEdit, db: Session = Depends(get_db)):
     db_user, db_workspace = get_workspace_edit(db=db, workspace_update=workspace_update)
     for user in db_workspace.users:
--- tests/test_workspaces.py
+++ tests/test_workspaces.py
@@ -5,24 +5,118 @@ from utils import temp_db
 
 client = TestClient(app)
 
-@temp_db
-def test_create_user():
-    response = client.post(
-        "/users",
+workspace = {
+    "name": "Get shit done Inc."
+}
+
+user = {
+    "email": "batman@gothem.com",
+    "password": "CrimeSucks33",
+    "first_name": "Clark",
+    "last_name": "Kent"
+}
+
+user2 = {
+    "email": "robin@gothem.com",
+    "password": "BatmanRocks1234",
+    "first_name": "Robin",
+    "last_name": "Hood"
+}
+
+def create_user(user):
+    return client.post(
+        "/user",
+        json=user,
+    )
+
+def read_user(id: int):
+    return client.get(f"/user/{id}")
+
+def create_workspace():
+    return client.post(
+        "/workspace",
         json={
-            "email": "batman@gothem.com",
-            "password": "CrimeSucks33",
-            "first_name": "Clark",
-            "last_name": "Kent"
+            "name": workspace["name"],
+            "user_id": 1
         },
     )
-    assert response.status_code == 200
-    assert response.json() == {
+
+def read_workspace(id: int):
+    return client.get(f"/workspace/{id}")
+
+def add_user_to_workspace(id: int, user_id: int):
+    return client.put(
+        "/workspace/user",
+        json={
+            "id": id,
+            "user_id": user_id
+        },
+    )
+
+def remove_user_from_workspace(id: int, user_id: int):
+    return client.delete(
+        "/workspace/user",
+        json={
+            "id": id,
+            "user_id": user_id
+        },
+    )
+
+@temp_db
+def test_create_and_get_user():
+    expected = {
         "id": 1,
-        "email": "batman@gothem.com",
-        "first_name": "Clark",
-        "last_name": "Kent",
+        "email": user["email"],
+        "first_name": user["first_name"],
+        "last_name": user["last_name"],
         "is_active": True,
         "role": None,
         "workspace": None
     }
+    response = create_user(user)
+    assert response.status_code == 200
+    assert response.json() == expected
+    response = read_user(1)
+    assert response.status_code == 200
+    assert response.json() == expected
+
+@temp_db
+def test_create_and_get_workspace():
+    expected = {
+        "id": 1,
+        "name": "Get shit done Inc.",
+        "is_active": True,
+        "users": [
+            {
+            "id": 1,
+            "email": user["email"],
+            "first_name": user["first_name"],
+            "last_name": user["last_name"],
+            "is_active": True,
+            "role": None,
+            "workspace": 1
+            }
+        ]
+    }
+    create_user(user)
+    response = create_workspace()
+    assert response.status_code == 200
+    assert response.json() == expected
+    response = read_workspace(1)
+    assert response.status_code == 200
+    assert response.json() == expected
+
+
+@temp_db
+def test_multiuser_workspace():
+    create_user(user)
+    response =create_workspace()
+    assert response.status_code == 200
+    assert len(response.json()['users']) == 1
+    create_user(user2)
+    response = add_user_to_workspace(id=1, user_id=2)
+    assert response.status_code == 200
+    assert len(response.json()['users']) == 2
+    response = remove_user_from_workspace(id=1, user_id=2)
+    assert response.status_code == 200
+    assert len(response.json()['users']) == 1
